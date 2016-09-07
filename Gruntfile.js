5d34d3d878f6dd4741dcd3f5956139fde8c15da8
Jamie Duke <jamsduke@gmail.com>
2016-09-07 16:04:40 -0500

    changed file system, fixed grunt, base functionality of app achieved

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,38 +2,25 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     // Change the b-fy task to add a transform task
-    browserify: {
-      js: {
-          src: ['./js/main.js'],
-          dest: 'dist/app.js'
-      },
-      options: {
-          transform: ['hbsfy']
-      }
-    },
     jshint: {
       options: {
-        predef: [ "document", "console", "$" ],
+        predef: [ "document", "console", "$", "alert" ],
         esnext: true,
         globalstrict: true,
         globals: {},
         browserify: true
       },
-      files: ['./js/**/*.js']
+      files: ['quiz.js']
     },
     watch: {
       javascripts: {
-        files: ['./js/**/*.js'],
-        tasks: ['jshint', 'browserify']
-      },
-      hbs: {
-        files: ['./templates/**/*.hbs'],
-        tasks: ['browserify']
+        files: ['.js'],
+        tasks: ['jshint']
       }
     }
   });
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('default', ['jshint', 'browserify', 'watch']);
+  grunt.registerTask('default', ['jshint', 'watch']);
 };
